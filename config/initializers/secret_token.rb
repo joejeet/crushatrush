a307124dfcb3a1aca28ed7400a7332345914e370
joejeet <joejeet@yahoo.co.in>
2012-10-03 00:02:40 +0100

    basic structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crushatrush::Application.config.secret_token = '02009bc9840d5eab1efcba68a537b569b5aaf507b6fca40ed8e59964468c70a3e1ed9522bdb04510e44af7b7b44934302520b3a118dff086268237c285dd8ef3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Crushatrush::Application.config.session_store :cookie_store, key: '_crushatrush_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Crushatrush::Application.config.session_store :active_record_store
